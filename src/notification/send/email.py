9559353a95b9cedf455d528efd5a6acfa7451edd
Amin Farjadi <amin.farjadi@outlook.com>
2024-05-01 21:47:28 +0100

    Adding the notification service

---
+++ src/notification/send/email.py
@@ -0,0 +1,35 @@
+import smtplib, os, json
+from email.message import EmailMessage
+
+def notification(message):
+
+    try:
+        message = json.loads(message)
+        mp3_fid = message["mp3_fid"]
+        sender_address = os.environ.get("GMAIL_ADDRESS")
+        sender_password = os.environ.get("GMAIL_PASSWORD")
+        receiver_address = message["username"]
+        if not sender_address:
+            return Exception("Sender email address not found.")
+        if not sender_password:
+            return Exception("Sender email password not found.")
+        if not receiver_address:
+            return Exception("Receiver email address not provided.")
+
+        msg = EmailMessage()
+        msg.set_content(f"mp3 file_id: {mp3_fid} is now ready!")
+        msg["Subject"] = "MP3 Download"
+        msg["From"] = sender_address
+        msg["To"] = receiver_address
+
+        session = smtplib.SMTP("smtp.gmail.com")
+        session.starttls()
+        session.login(sender_address,sender_password)
+        session.send_message(msg,sender_address,receiver_address)
+        session.quit()
+
+        print("Email sent.")
+
+    except Exception as err:
+        print(f"{err}")
+        return err
